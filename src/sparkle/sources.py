0af19b5ceba9be0d2a8c7adceae64ad3f71693d9
Reza (Shahin) Khanipour <Shahin@DataChef.co>
2024-08-30 11:41:35 +0200

    refactor(Soruces): to preserve typing, documentation and autocompletion.
    With this method, it's possible to keep users flexible in defining the
    sources, but also provide them with enough DX while developing the
    application.

--- src/sparkle/sources.py
+++ src/sparkle/sources.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from enum import Enum
-from .reader import Reader
 from typing import Any
+from .reader import Reader
 
 
 class SourceType(Enum):
@@ -40,46 +40,14 @@ class Source:
             reader_config=source_dict["reader_config"],
         )
 
-
-class Sources:
-    """Class to simplify source access."""
-
-    def __init__(self, sources: list[Source]) -> None:
-        """Initialize the Sources object.
-
-        After initialization, the Sources object will have attributes
-        for each source. The properties will be named after the
-        source, and their values are actual readers pre-configured to
-        read from the target source type.
-
-        Args:
-            sources (list[SourceType]): List of sources
-
-        """
-        for source in sources:
-            self._add_source(source)
-
-    def _add_source(self, source: Source) -> None:
-        """Add a source to the Sources object.
-
-        Args:
-            source (SourceType): Source to add
-        """
-        if not isinstance(source, Source):
-            raise TypeError(f"Expected Source, got {type(source)}")
-
-        setattr(self, source.name, self._dataframe_reader(source))
-
-    def _dataframe_reader(self, source: Source) -> Reader:
-        """Create a DataFrame reader for a source.
-
-        Args:
-            source (Source): Source object
+    @property
+    def reader(self) -> Reader:
+        """The reader method creates a reader object based on the source type.
 
         Returns:
             Reader: Reader object
         """
-        match source.type:
+        match self.type:
             case SourceType.hive_table:
                 # TODO Create a Hive reader
                 raise NotImplementedError("Hive reader not implemented")
@@ -92,3 +60,26 @@ class Sources:
             case SourceType.s3_bucket:
                 # TODO Create an S3 reader
                 raise NotImplementedError("S3 reader not implemented")
+
+
+@dataclass
+class SourcesModel:
+    """Dataclass model for source validation.
+
+    This class is used to validate that all properties of a class are
+    of type Source. To use this class, inherit from it and define the
+    properties as Source objects. For example:
+
+    @dataclass
+    class Sources(SourcesModel):
+        f1: Source
+        f2: Source
+
+    """
+
+    def __post_init__(self):
+        """Validate that all properties are of type Source."""
+        for prop in self.__annotations__:
+            type_ = type(getattr(self, prop))
+            if not isinstance(type_, Source):
+                raise TypeError(f"Expected Source, got {type_}")
