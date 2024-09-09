c1ba121e85fecde6f199cc4df2dfa21465422e42
Farbod Ahmadian <farbodahmadian2014@gmail.com>
2024-09-09 15:51:46 +0200

    feat: Add Kafka stream publisher

--- src/sparkle/config/__init__.py
+++ src/sparkle/config/__init__.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
-from .kafka_config import KafkaConfig
+from .kafka_config import KafkaReaderConfig, KafkaWriterConfig
 from .iceberg_config import IcebergConfig
 from .database_config import TableConfig
+import os
 
 
 @dataclass(frozen=True)
@@ -12,7 +13,24 @@ class Config:
     app_id: str
     version: str
     database_bucket: str
-    kafka: KafkaConfig | None
-    hive_table_input: TableConfig | None
-    iceberg_output: IcebergConfig | None
+    checkpoints_bucket: str
+    filesystem_scheme: str = "s3a://"
     spark_trigger: str = '{"once": True}'
+    kafka_input: KafkaReaderConfig | None = None
+    kafka_output: KafkaWriterConfig | None = None
+    hive_table_input: TableConfig | None = None
+    iceberg_output: IcebergConfig | None = None
+
+    @property
+    def checkpoint_location(self):
+        """Path to the s3 location where checkpoints are stored."""
+        if not self.kafka_input or not self.kafka_output:
+            raise ValueError("Kafka input and output configurations are required.")
+
+        return os.path.join(
+            f"{self.filesystem_scheme}{self.checkpoints_bucket}",
+            "spark",
+            self.app_id,
+            self.kafka_input.kafka_topic,
+            self.kafka_output.kafka_topic,
+        )
