ed64e749d43275d5a5b00ad8af1081660169e7d6
Farbod Ahmadian <farbodahmadian2014@gmail.com>
2024-09-10 12:48:43 +0200

    feat: add avro parser

--- src/sparkle/utils/spark.py
+++ src/sparkle/utils/spark.py
@@ -1,5 +1,8 @@
 from sparkle.utils.logger import logger
 from pyspark.sql import SparkSession, functions as F, DataFrame
+from pyspark.sql.avro.functions import from_avro
+from sparkle.reader.schema_registry import SchemaRegistry
+from collections.abc import Callable
 
 
 def table_exists(
@@ -61,3 +64,57 @@ def to_kafka_dataframe(unique_identifier_column_name: str, df: DataFrame) -> Dat
             F.to_json(F.struct([df[f] for f in df.columns])).alias("value"),
         ]
     )
+
+
+def parse_by_avro(
+    topic: str,
+    schema_registry_client: SchemaRegistry,
+    options: dict[str, str] = dict(),
+) -> Callable[[DataFrame], DataFrame]:
+    """Parses Kafka messages in Avro format using a schema from the schema registry.
+
+    This function generates a transformer function that can be applied to a Spark DataFrame
+    containing Kafka messages. It uses a schema fetched from the schema registry to parse
+    the Avro-encoded message values.
+
+    Args:
+        topic (str): The Kafka topic name to fetch the schema for.
+        schema_registry_client (SchemaRegistry): Client to interact with the schema registry.
+        options (dict[str, str], optional): Additional options for Avro parsing. Defaults to an empty dictionary.
+
+    Returns:
+        Callable[[DataFrame], DataFrame]: A transformer function that takes a Spark DataFrame
+        and returns a DataFrame with parsed Avro values.
+
+    Example:
+        >>> transformer = parse_by_avro("my_topic", schema_registry_client)
+        >>> transformed_df = transformer(input_df)
+
+    Raises:
+        ValueError: If the schema cannot be fetched from the schema registry.
+    """
+    schema = schema_registry_client.cached_schema(topic)
+
+    # Skip the first 5 bytes of the value, which is the magic byte and
+    # the schema ID. The rest is the Avro value.
+    avro_value = F.expr("substring(value, 6, length(value))")
+
+    def transformer(df: DataFrame) -> DataFrame:
+        kafka_metadata = "__kafka_metadata__"
+        return (
+            df.withColumn(
+                kafka_metadata,
+                F.struct(
+                    df.key,
+                    df.topic,
+                    df.partition,
+                    df.offset,
+                    df.timestamp,
+                    df.timestampType,
+                ),
+            )
+            .withColumn("value", from_avro(avro_value, schema, options=options))
+            .select("value.*", kafka_metadata)
+        )
+
+    return transformer
