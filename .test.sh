900d0259dd0f55c3a2883a148d4366fd7c3fa5e1
Farbod Ahmadian <farbodahmadian2014@gmail.com>
2024-09-11 16:17:01 +0200

    fix: wait for schema registry to be ready before running tests

--- .test.sh
+++ .test.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+SERVICE_NAME="schema-registry"
+
+# Loop until the service is running
+while true; do
+  # Check if the service is running
+  if [ -z $(docker compose -f tests/docker-compose.yml ps -q $SERVICE_NAME) ] || [ -z $(docker ps -q --no-trunc | grep $(docker compose ps -q $SERVICE_NAME)) ]; then
+    echo "No, $SERVICE_NAME is not running."
+    sleep 5
+  else
+    echo "Yes, $SERVICE_NAME is running."
+    break
+  fi
+done
+
+
 python -m mypy .
 python -m pytest -vv
