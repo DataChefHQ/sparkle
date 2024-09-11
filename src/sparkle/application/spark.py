28f9013d0c590babf9ffc95449c9232ccac44b4f
Reza (Shahin) Khanipour <Shahin@DataChef.co>
2024-09-11 14:25:04 +0200

    fix(spark): lock package version, to ensure they match plugins.

--- src/sparkle/application/spark.py
+++ src/sparkle/application/spark.py
@@ -12,6 +12,7 @@ except ImportError:
 _SPARK_EXTENSIONS = [
     "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions",
 ]
+
 _SPARK_PACKAGES = [
     "org.apache.iceberg:iceberg-spark-runtime-3.3_2.12:1.3.1",
     "org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0",
