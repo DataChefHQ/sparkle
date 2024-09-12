727cebe50a6c64465395eff38ace273618670a57
Farbod Ahmadian <farbodahmadian2014@gmail.com>
2024-09-12 10:08:14 +0200

    feat: wait for schema registry to become available

--- .test.sh
+++ .test.sh
@@ -3,18 +3,28 @@ set -euo pipefail
 
 SERVICE_NAME="schema-registry"
 
-# Loop until the service is running
-while true; do
-  # Check if the service is running
-  if [ -z $(docker compose -f tests/docker-compose.yml ps -q $SERVICE_NAME) ] || [ -z $(docker ps -q --no-trunc | grep $(docker compose ps -q $SERVICE_NAME)) ]; then
-    echo "No, $SERVICE_NAME is not running."
-    sleep 5
-  else
-    echo "Yes, $SERVICE_NAME is running."
-    break
-  fi
-done
+# Function to check if the container is healthy
+check_health() {
+    STATUS=$(docker inspect --format='{{.State.Health.Status}}' schema-registry 2>/dev/null)
 
+    if [ "$STATUS" == "healthy" ]; then
+        echo "Container 'schema-registry' is healthy."
+        return 0
+    else
+        echo "Container 'schema-registry' is not healthy. Current status: $STATUS"
+        return 1
+    fi
+}
+
+# Loop until the container is healthy
+while true; do
+    if check_health; then
+        break
+    else
+        echo "Retrying in 5 seconds..."
+        sleep 5
+    fi
+done
 
 python -m mypy .
 python -m pytest -vv
