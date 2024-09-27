7791ae0f811b0b1d6fc388a671b6ff7784ca5241
Federico Zambelli <federico@datachef.co>
2024-09-27 16:32:36 +0200

    fix: add missing colon

--- src/sparkle/writer/kafka_writer.py
+++ src/sparkle/writer/kafka_writer.py
@@ -183,7 +183,7 @@ class KafkaBatchPublisher(Writer):
         # Convert the DataFrame to a Kafka-friendly format
         kafka_df = to_kafka_dataframe(self.unique_identifier_column_name, df)
 
-        if set(kafka_df.columns) != {"key", "value"}
+        if set(kafka_df.columns) != {"key", "value"}:
             raise KeyError(
                 "The DataFrame must contain 'key' and 'value' columns. "
                 "Ensure that `to_kafka_dataframe` transformation is correctly applied."
